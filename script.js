16e3a6c2cfa6799cf331b82b90a8616a541ca285
seaweed <132213277+seaweed32@users.noreply.github.com>
2024-01-16 19:36:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function getDay(){
+    const now = new Date();
+    const startOfYear = new Date(now.getFullYear(), 0, 0);
+    const diff = now - startOfYear;
+    const dayOfYear = Math.floor(diff / (24 * 60 * 60 * 1000));
+    return dayOfYear;
+  }
+  
+  const dayOfYear = getDay();
+  let text = "It is New Year's ";
+  let daysTil = 365-dayOfYear;
+  
+  if(dayOfYear < 365){
+    for(let i=0; i<daysTil; i++){
+      text += "Eve "
+    }
+  } else {
+    text += "Day";
+  }
+  
+  document.getElementById("output_box").innerHTML = text;
